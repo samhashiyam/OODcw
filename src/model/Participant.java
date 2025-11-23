a3b6f9f6894a58fe2219ba9c8bf80f4e167722bd
samhashiyam <samhashiyam22@gmail.com>
2025-11-23 13:26:36 +0530

    Added participant class

--- src/model/Participant.java
+++ src/model/Participant.java
@@ -1,5 +1,63 @@
 package model;
 
 public class Participant {
-    private
+    private String id;
+    private String name;
+    private String email;
+    private String prefferedGame;
+    private int skillLevel;
+    private String prefferedRole;
+    private int personalityScore;
+    private String personalityType;
+
+    public Participant(String id, String name, String email, String preferredGame,
+                       int skillLevel, String preferredRole, int personalityScore,
+                       String personalityType){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.prefferedGame = preferredGame;
+        this.skillLevel = skillLevel;
+        this.prefferedRole = preferredRole;
+        this.personalityScore = personalityScore;
+        this.personalityType = personalityType;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPrefferedGame() {
+        return prefferedGame;
+    }
+
+    public int getSkillLevel() {
+        return skillLevel;
+    }
+
+    public String getPrefferedRole() {
+        return prefferedRole;
+    }
+
+    public int getPersonalityScore() {
+        return personalityScore;
+    }
+
+    public String getPersonalityType() {
+        return personalityType;
+    }
+
+    @Override
+    public String toString() {
+        return id +"(" + personalityType + ") - " + prefferedGame;
+    }
 }
